990c1668189ab7a7549d0678595694f8bf18af6a
Satendra Singh <satendrasingh1325@gmail.com>
2023-09-27 20:11:42 +0530

    setup React/Tailwind and inital Pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "./features/counter/Counter";
+import ProductList from "./features/product-list/ProductList";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
